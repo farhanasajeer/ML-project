58a4a054448613ae9acd89a76f24a21cbbec3888
farhanasajeer <shirinumer07@gmail.com>
2022-03-09 16:17:28 +0530

    social commit

---
+++ Breast canser/deploy.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request
+import pickle
+import numpy as np
+app = Flask(__name__)
+model=pickle.load(open('model.pkl','rb'))
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    int_features=[int(x) for x in request.form.values()]
+    final_features = np.array(int_features).reshape(1,9)
+    print(final_features)
+    prediction = model.predict(final_features)
+    L_collection = {0: "No Recurrense events ", 1: "Recurrense events"}
+    result=L_collection[prediction[0]]
+    print(result)
+    
+    return render_template('result.html', prediction_text="YOU HAVE  {}".format(result))
+
+if __name__=='__main__':
+    app.run(port=5000)
+
